44fdb19b4cfe3ce297593e0a86fa1c275620a14c
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2016-03-05 12:10:42 +0400

    create sales_4

--- sales_analysis.R
+++ sales_analysis.R
@@ -4,36 +4,59 @@ sales_path = '/Users/nyuad/Documents/workspace/Datathon/datathon data/sales/'
 
 sales_full = read.csv(paste(sales_path, 'sales.csv', sep=''), stringsAsFactors=F)
 sales_full_3 = read.csv(paste(sales_path, 'sales_3.csv', sep=''), stringsAsFactors=F)
-sales_full_k = read.csv(paste(sales_path, 'sales-kmistry-hot-n-heavy-remix.csv', sep=''), stringsAsFactors=F)
-sales_full_k = sales_full_k[c('item', 'family', 'group', 'qnty.sold', 'revenue', 'date', 'place')]
+sales_full_k = read.csv(paste(sales_path, 'sales_3-kmistry-f-yacout-remix.csv', sep=''), stringsAsFactors=F)
 
 # Pre-processing
-
 sales = na.omit(sales_full)
 sales_3 = na.omit(sales_full_3)
 sales_k = na.omit(sales_full_k)
 
-sales = 
+# Recover FOOD family from sales
+food_fam = sales[sales$family=='FOOD',]
+split_name_list = lapply(strsplit(food_fam$item, ","), function(x) gsub("^\\s+|\\s+$", "", x))
+split_name = data.frame(matrix(ncol = 2, nrow = length(split_name_list)))
 
-sales$date = as.Date(sales$date, '%Y-%m-%d')
-sales$weekday = weekdays(sales$date)
-sales$month = month(sales$date)
+for(ii in 1:length(split_name_list)){
+	split_name[ii,1] = split_name_list[[ii]][1]
+	split_name[ii,2] = split_name_list[[ii]][2]
+}
+colnames(split_name) = c('item', 'family')
 
-# Keep only dining hall food
+food_fam$revenue = food_fam$qnty.sold
+food_fam$qnty.sold = food_fam$group
+food_fam$group = food_fam$family
+food_fam$family = split_name$family
+food_fam$item = split_name$item
+
+# Add recovered data to sales_3
+sales_3[sales_3$family == 'FOOD',] = food_fam
+sales_4 = sales_3[-which(sales_3$family == 'FOOD'),]
+sales_4 = rbind(sales_4, food_fam)
+
+# Add weekday and month data
+sales_4$date = as.Date(sales_4$date, '%Y-%m-%d')
+sales_4$weekday = weekdays(sales_4$date)
+sales_4$month = month(sales_4$date)
+
+
 # whatever appears less than 5 time
 # in food family clean out cs
 
 #unique_families = unique(sales$family)
 #write.csv(unique_families,'/Users/nyuad/Documents/workspace/Datathon/datathon data/sales/unique_families.csv',row.names=F)
 
+# Separate dining hall food
 
-selected_families = read.csv('/Users/nyuad/Documents/repos/diningDataAnalysis/unique_families3.csv',stringsAsFactors=F, header=F)
+selected_families_dh = read.csv('/Users/nyuad/Documents/repos/diningDataAnalysis/unique_families3.csv',stringsAsFactors=F, header=F)
 colnames(selected_families) = 'Family'
 
-food = sales[sales$family %in% selected_families$Family,]
-food_3 = sales_3[sales_3$family %in% selected_families$Family,]
+dh_food = sales_4[sales_4$family %in% selected_families$Family,]
+
+# Separate Market Place
+mp_food = sales_k[]
 
 
+# Separate Market Place food
 
 
 all_dates = 
@@ -55,6 +78,7 @@ day_intervals = difftime(unique_dates[2:end(unique_dates)],unique_dates[1:end(un
 
 unique_families = unique(sales$family)
 write.csv(unique_families,'/Users/nyuad/Documents/workspace/Datathon/datathon data/sales/unique_families.csv',row.names=F)
+write.csv(sales_4,'/Users/nyuad/Documents/workspace/Datathon/datathon data/sales/sales_4.csv',row.names=F)
 
 #November 10 - spike in all food
 
@@ -65,3 +89,4 @@ write.csv(unique_families,'/Users/nyuad/Documents/workspace/Datathon/datathon da
 
 
 #subset data based on food and date
+
